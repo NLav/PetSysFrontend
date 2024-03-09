cbd14f080575fd36c92fa37c8457e73cf02586d0
Nicolas <nicolas.lavoura@teamsoft.com.br>
2024-03-09 01:29:43 -0300

    make component Menu to be collapsable

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,29 +1,54 @@
-import { Dog, PawPrint, Users, UsersFour } from "@phosphor-icons/react";
+import {
+  CaretDoubleLeft,
+  Dog,
+  PawPrint,
+  Users,
+  UsersFour,
+} from "@phosphor-icons/react";
+import { useState } from "react";
 import "./Menu.scss";
 
 const Menu = () => {
-  return (
-    <div className="menu">
-      <div className="menu__logo-container">
-        <PawPrint size={120} />
+  const [collapseMenu, setCollapseMenu] = useState(false);
 
-        <span>Meu Pet</span>
+  return (
+    <div
+      className={`menu ${collapseMenu ? "menu--collapsed" : null}`}
+      onClick={() => setCollapseMenu(!collapseMenu)}
+    >
+      <button
+        className="menu__collapse-button"
+        onClick={() => setCollapseMenu(!collapseMenu)}
+      >
+        <CaretDoubleLeft
+          size={16}
+          weight="bold"
+          style={{ rotate: collapseMenu ? "180deg" : "0deg" }}
+        />
+      </button>
+
+      <div className="menu__logo-container">
+        <PawPrint size={collapseMenu ? 64 : 120} />
+
+        {!collapseMenu ? "Meu Pet" : null}
       </div>
 
-      <div className="menu__navigation-list">
+      <div
+        className={`menu__navigation-list ${collapseMenu && "menu__navigation-list--collapsed"}`}
+      >
         <button>
-          <Dog size={24} />
-          Pets
+          <Dog size={32} />
+          {!collapseMenu ? "Pets" : null}
         </button>
 
         <button>
           <Users size={32} />
-          Tutores
+          {!collapseMenu ? "Tutores" : null}
         </button>
 
         <button>
           <UsersFour size={32} />
-          Funcionários
+          {!collapseMenu ? "Funcionários" : null}
         </button>
       </div>
     </div>
