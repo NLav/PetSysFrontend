da859bbc749219004a971f60d02d8f4f4f1c6fa2
Nicolas <nicolas.lavoura@teamsoft.com.br>
2024-03-17 18:52:15 -0300

    improve Pets listing and add text-overflow to CardPet

--- src/components/CardPet/CardPet.tsx
+++ src/components/CardPet/CardPet.tsx
@@ -16,13 +16,27 @@ const CardPet = ({ name, imageUrl, birthDate, breed }: ICardPetProps) => {
         <CardImage imageUrl={imageUrl} alt={`${name}-picture`} />
       </span>
 
-      <span className="card-pet__name">{name}</span>
+      <div className="card-pet__information-container card-pet__name-container">
+        <span>Nome:</span>
 
-      <span className="card-pet__age">
-        {format(birthDate || new Date(2020, 3, 1), "dd/MM/yyyy")}
+        <span className="card-pet__name-container__name" data-tooltip="name">
+          {name}
+        </span>
+      </div>
+
+      <span className="card-pet__information-container card-pet__age-container">
+        <span>Data de nascimento:</span>
+
+        <span className="card-pet__age-container__age">
+          {format(birthDate || new Date(2020, 3, 1), "dd/MM/yyyy")}
+        </span>
       </span>
 
-      <span className="card-pet__breed">{breed}</span>
+      <span className="card-pet__information-container card-pet__breed-container">
+        <span>Raça:</span>
+
+        <span className="card-pet__breed-container__breed">{breed}</span>
+      </span>
     </div>
   );
 };
