e347f176f6c1bc20e72727a8dda269d2f2bee068
Nicolas <nicolas.lavoura@teamsoft.com.br>
2024-03-24 00:11:22 -0300

    add necessary redirects

--- src/layouts/DefaultLayout/DefaultLayout.tsx
+++ src/layouts/DefaultLayout/DefaultLayout.tsx
@@ -1,8 +1,20 @@
 import { Menu } from "components";
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { LocalStorageService } from "services";
 import * as S from "./DefaultLayout.styles";
 
 const DefaultLayout = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!LocalStorageService.getAccessToken()) {
+      navigate("/login");
+    } else if (window.location.pathname === "/") {
+      navigate("/pets");
+    }
+  }, [navigate]);
+
   return (
     <S.Container>
       <Menu />
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,5 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+import { LocalStorageService } from "./LocalStorageService";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
@@ -10,11 +11,31 @@ api.interceptors.request.use(
       config.headers["force-origin"] = import.meta.env.VITE_FORCE_ORIGIN;
     }
 
+    config.headers["Authorization"] =
+      `Bearer ${LocalStorageService.getAccessToken()}`;
+
     return config;
   },
 
   (error) => {
     console.error(error);
+  }
+);
+
+api.interceptors.response.use(
+  (config) => {
+    return config;
+  },
+  (error: AxiosError) => {
+    console.error(error);
+
+    if (
+      error.response?.status === 401 &&
+      LocalStorageService.getAccessToken()
+    ) {
+      LocalStorageService.deleteAccessToken();
+      window.location.href = "/login";
+    }
   }
 );
 
